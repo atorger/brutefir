c5443b01ff5c4d42b5fe43dc0487cb77d1e5ff8c
Anders Torger <anders@torger.se>
2025-11-17 21:19:53 +0100

    C99 cleanup

--- fdrw.h
+++ fdrw.h
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2000, 2001, 2002 -- Anders Torger
+ * (c) Copyright 2000, 2001, 2002, 2025 -- Anders Torger
  *
  * This program is open source. For license terms, see the LICENSE file.
  *
@@ -7,15 +7,14 @@
 #ifndef FDRW_H_
 #define FDRW_H_
 
+#include <stdbool.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 
-#include "defs.h"
-
-static inline bool_t
+static inline bool
 readfd(int fd,
        void *buf,
        size_t count)
@@ -25,8 +24,7 @@ readfd(int fd,
     do {
 	if ((n = read(fd, &((uint8_t *)buf)[i], count - i)) < 1) {
 	    if (n == 0 || errno != EINTR) {
-		fprintf(stderr, "(%d) read from fd %d failed: %s\n",
-                        (int)getpid(), fd, strerror(errno));
+		fprintf(stderr, "read from fd %d failed: %s\n", fd, strerror(errno));
 		return false;
 	    }
 	    continue;
@@ -36,7 +34,7 @@ readfd(int fd,
     return true;
 }
 
-static inline bool_t
+static inline bool
 writefd(int fd,
 	const void *buf,
 	size_t count)
@@ -46,8 +44,7 @@ writefd(int fd,
     do {
 	if ((n = write(fd, &((const uint8_t *)buf)[i], count - i)) < 1) {
 	    if (n == 0 || errno != EINTR) {
-		fprintf(stderr, "(%d) write to fd %d failed: %s\n",
-                        (int)getpid(), fd, strerror(errno));
+		fprintf(stderr, "write to fd %d failed: %s\n", fd, strerror(errno));
 		return false;
 	    }
 	    continue;
