9f36b4d64fb99580a38d49a86c3d8ed9068e4949
Anders Torger <anders@torger.se>
2025-11-17 21:47:28 +0100

    C99 cleanup

--- inout.h
+++ inout.h
@@ -1,19 +1,17 @@
 /*
- * (c) Copyright 2001 -- Anders Torger
+ * (c) Copyright 2001, 2025 -- Anders Torger
  *
  * This program is open source. For license terms, see the LICENSE file.
  *
  */
-#ifndef _INOUT_H_
-#define _INOUT_H_
+#ifndef INOUT_H_
+#define INOUT_H_
 
 #include "bfmod.h"
 
 #define IN  BF_IN
 #define OUT BF_OUT
 
-#define FOR_IN_AND_OUT for (IO = 0; IO < 2; IO++)
-
-extern int IO;
+#define FOR_IN_AND_OUT for (int IO = 0; IO < 2; IO++)
 
 #endif
