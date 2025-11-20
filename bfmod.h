1dc0323c5b04afae161ddd9eaaaca76e80bf90ec
Anders Torger <anders@torger.se>
2025-11-20 19:30:00 +0100

    untabify

--- bfmod.h
+++ bfmod.h
@@ -4,8 +4,8 @@
  * This program is open source. For license terms, see the LICENSE file.
  *
  */
-#ifndef _BFMOD_H_
-#define _BFMOD_H_
+#ifndef BFMOD_H_
+#define BFMOD_H_
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,7 +18,7 @@ extern "C" {
 
 #define BF_VERSION_MAJOR 2
 #define BF_VERSION_MINOR 0
-    
+
 /* limits */
 #define BF_MAXCHANNELS 256
 #define BF_MAXFILTERS 256
@@ -49,7 +49,7 @@ extern "C" {
 
 #define BF_SAMPLE_FORMAT_MAX BF_SAMPLE_FORMAT_FLOAT64_BE
 
-/* macro sample formats */    
+/* macro sample formats */
 #define BF_SAMPLE_FORMAT_S16_NE 16
 #define BF_SAMPLE_FORMAT_S16_4NE 17 // unused since 1.0m
 #define BF_SAMPLE_FORMAT_S24_NE 18
@@ -59,7 +59,7 @@ extern "C" {
 #define BF_SAMPLE_FORMAT_FLOAT64_NE 22
 #define BF_SAMPLE_FORMAT_AUTO 23
 
-#define BF_SAMPLE_FORMAT_MACRO_MAX 23    
+#define BF_SAMPLE_FORMAT_MACRO_MAX 23
 
 /* exit values */
 #define BF_EXIT_OK 0
@@ -73,8 +73,8 @@ extern "C" {
 #define BF_CALLBACK_EVENT_NORMAL 0
 #define BF_CALLBACK_EVENT_ERROR 1
 #define BF_CALLBACK_EVENT_LAST_INPUT 2
-#define BF_CALLBACK_EVENT_FINISHED 3    
-    
+#define BF_CALLBACK_EVENT_FINISHED 3
+
 #define BF_LEX_EOS     1 /* end of statement (;) */
 #define BF_LEX_LBRACE  2 /* { */
 #define BF_LEX_RBRACE  3 /* } */
@@ -88,7 +88,7 @@ extern "C" {
 
 #define BF_SAMPLE_SLOTS 100
 #define BF_UNDEFINED_SUBDELAY (-BF_SAMPLE_SLOTS)
-    
+
 union bflexval {
     double real;
     int boolean;
@@ -145,27 +145,27 @@ struct bfaccess {
  * nothing happens.
  */
     void (*toggle_mute)(int io,
-			int channel);  
+                        int channel);
     int (*ismuted)(int io,
-		   int channel);
-    
+                   int channel);
+
 /*
  * Change delay of the given channel. If the delay or channel is out of range,
  * -1 is returned, else 0.
  */
     int (*set_delay)(int io,
-		     int channel,
-		     int delay);
+                     int channel,
+                     int delay);
     int (*get_delay)(int io,
-		     int channel);
+                     int channel);
 
     double (*realtime_index)(void);
 
     const char **(*bfio_names)(int io,
-			       int *n_names);
+                               int *n_names);
     void (*bfio_range)(int io,
-		       int modindex,
-		       int range[2]);
+                       int modindex,
+                       int range[2]);
     int (*bfio_command)(int io,
                         int modindex,
                         const char params[],
@@ -191,7 +191,7 @@ struct bfaccess {
 
 struct bfevents {
 #define BF_FDEVENT_PEAK 0x1
-#define BF_FDEVENT_INITIALISED 0x2    
+#define BF_FDEVENT_INITIALISED 0x2
     unsigned int fdevents;
     void (*peak)(void);
     void (*initialised)(void);
@@ -199,19 +199,19 @@ struct bfevents {
                         unsigned int block_index,
                         struct timeval *current_time);
     void (*input_timed)(void *buf,
-			int channel);
+                        int channel);
     void (*input_freqd)(void *buf,
-			int channel);
+                        int channel);
     void (*coeff_final)(int filter,
                         int *coeff);
     void (*pre_convolve)(void *buf,
-			 int filter);
+                         int filter);
     void (*post_convolve)(void *buf,
-			  int filter);
+                          int filter);
     void (*output_freqd)(void *buf,
-			 int channel);
+                         int channel);
     void (*output_timed)(void *buf,
-			 int channel);
+                         int channel);
 };
 
 #define BF_FUN_BFIO_ISCALLBACK  "bfio_iscallback"
@@ -241,15 +241,15 @@ struct bfio_module {
                      struct sched_param *callback_sched,
                      int debug);
     int (*init)(void *params,
-		int io,
-		int sample_format,
-		int sample_rate,
-		int open_channels,
-		int used_channels,
-		const int channel_selection[],
-		int period_size,
-		int *device_period_size,
-		int *isinterleaved,
+                int io,
+                int sample_format,
+                int sample_rate,
+                int open_channels,
+                int used_channels,
+                const int channel_selection[],
+                int period_size,
+                int *device_period_size,
+                int *isinterleaved,
                 void *callback_state,
                 int (*process_callback)(void **callback_states[2],
                                         int callback_state_count[2],
@@ -260,13 +260,13 @@ struct bfio_module {
                    const char params[]);
     /* see errno handling in dai_input in order to provide correct errnos */
     int (*read)(int fd,
-		void *buf,
-		int offset,
-		int count);
+                void *buf,
+                int offset,
+                int count);
     int (*write)(int fd,
-		 const void *buf,
-		 int offset,
-		 int count);
+                 const void *buf,
+                 int offset,
+                 int count);
     int (*synch_start)(void);
     void (*synch_stop)(void);
     int (*start)(int io);
@@ -304,16 +304,16 @@ struct bflogic_module {
                    int *fork_mode,
                    int debug);
     int (*init)(struct bfaccess *bfaccess,
-		int sample_rate,
-		int block_length,
-		int n_maxblocks,
-		int n_coeffs,
-		const struct bfcoeff coeffs[],
-		const int n_channels[2],
-		const struct bfchannel *channels[2],
-		int n_filters,
-		const struct bffilter filters[],
-		int event_fd,
+                int sample_rate,
+                int block_length,
+                int n_maxblocks,
+                int n_coeffs,
+                const struct bfcoeff coeffs[],
+                const int n_channels[2],
+                const struct bfchannel *channels[2],
+                int n_filters,
+                const struct bffilter filters[],
+                int event_fd,
                 int synch_fd);
     int (*command)(const char params[]);
     const char *(*message)(void);
@@ -324,37 +324,37 @@ bf_sampleformat_size(int format)
 {
     switch (format) {
     case BF_SAMPLE_FORMAT_S8:
-	return 1;
+        return 1;
     case BF_SAMPLE_FORMAT_S16_LE:
-	return 2;
+        return 2;
     case BF_SAMPLE_FORMAT_S16_BE:
-	return 2;
+        return 2;
     case BF_SAMPLE_FORMAT_S16_4LE:
-	return 4;
+        return 4;
     case BF_SAMPLE_FORMAT_S16_4BE:
-	return 4;
+        return 4;
     case BF_SAMPLE_FORMAT_S24_LE:
-	return 3;
+        return 3;
     case BF_SAMPLE_FORMAT_S24_BE:
-	return 3;
+        return 3;
     case BF_SAMPLE_FORMAT_S24_4LE:
-	return 4;
+        return 4;
     case BF_SAMPLE_FORMAT_S24_4BE:
-	return 4;
+        return 4;
     case BF_SAMPLE_FORMAT_S32_LE:
-	return 4;
+        return 4;
     case BF_SAMPLE_FORMAT_S32_BE:
-	return 4;
+        return 4;
     case BF_SAMPLE_FORMAT_FLOAT_LE:
-	return 4;
+        return 4;
     case BF_SAMPLE_FORMAT_FLOAT_BE:
-	return 4;
+        return 4;
     case BF_SAMPLE_FORMAT_FLOAT64_LE:
-	return 8;
+        return 8;
     case BF_SAMPLE_FORMAT_FLOAT64_BE:
-	return 8;
+        return 8;
     default:
-	return 0;
+        return 0;
     }
 }
 
@@ -363,53 +363,53 @@ bf_strsampleformat(int format)
 {
     switch (format) {
     case BF_SAMPLE_FORMAT_S8:
-	return "S8";
+        return "S8";
     case BF_SAMPLE_FORMAT_S16_LE:
-	return "S16_LE";
+        return "S16_LE";
     case BF_SAMPLE_FORMAT_S16_BE:
-	return "S16_BE";
+        return "S16_BE";
     case BF_SAMPLE_FORMAT_S16_NE:
-	return "S16_NE";
+        return "S16_NE";
     case BF_SAMPLE_FORMAT_S16_4LE:
-	return "S16_4LE";
+        return "S16_4LE";
     case BF_SAMPLE_FORMAT_S16_4BE:
-	return "S16_4BE";
+        return "S16_4BE";
     case BF_SAMPLE_FORMAT_S16_4NE:
-	return "S16_4NE";
+        return "S16_4NE";
     case BF_SAMPLE_FORMAT_S24_LE:
-	return "S24_LE";
+        return "S24_LE";
     case BF_SAMPLE_FORMAT_S24_BE:
-	return "S24_BE";
+        return "S24_BE";
     case BF_SAMPLE_FORMAT_S24_NE:
-	return "S24_NE";
+        return "S24_NE";
     case BF_SAMPLE_FORMAT_S24_4LE:
-	return "S24_4LE";
+        return "S24_4LE";
     case BF_SAMPLE_FORMAT_S24_4BE:
-	return "S24_4BE";
+        return "S24_4BE";
     case BF_SAMPLE_FORMAT_S24_4NE:
-	return "S24_4NE";
+        return "S24_4NE";
     case BF_SAMPLE_FORMAT_S32_LE:
-	return "S32_LE";
+        return "S32_LE";
     case BF_SAMPLE_FORMAT_S32_BE:
-	return "S32_BE";
+        return "S32_BE";
     case BF_SAMPLE_FORMAT_S32_NE:
-	return "S32_NE";
+        return "S32_NE";
     case BF_SAMPLE_FORMAT_FLOAT_LE:
-	return "FLOAT_LE";
+        return "FLOAT_LE";
     case BF_SAMPLE_FORMAT_FLOAT_BE:
-	return "FLOAT_BE";
+        return "FLOAT_BE";
     case BF_SAMPLE_FORMAT_FLOAT_NE:
-	return "FLOAT_NE";
+        return "FLOAT_NE";
     case BF_SAMPLE_FORMAT_FLOAT64_LE:
-	return "FLOAT64_LE";
+        return "FLOAT64_LE";
     case BF_SAMPLE_FORMAT_FLOAT64_BE:
-	return "FLOAT64_BE";
+        return "FLOAT64_BE";
     case BF_SAMPLE_FORMAT_FLOAT64_NE:
-	return "FLOAT64_NE";
+        return "FLOAT64_NE";
     case BF_SAMPLE_FORMAT_AUTO:
         return "AUTO";
     default:
-	return "##unknown sample format##";
+        return "##unknown sample format##";
     }
 }
 
